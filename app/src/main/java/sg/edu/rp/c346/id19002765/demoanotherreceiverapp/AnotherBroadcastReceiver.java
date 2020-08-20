936b62e16a348fe0fd88c15b5fe4a8316152bf1b
TAN DAVID <19002765@myrp.edu.sg>
2020-08-20 10:08:34 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id19002765/demoanotherreceiverapp/AnotherBroadcastReceiver.java
@@ -0,0 +1,13 @@
+package sg.edu.rp.c346.id19002765.demoanotherreceiverapp;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+public class AnotherBroadcastReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "Received in AnotherBroadcastReceiver", Toast.LENGTH_LONG).show();
+    }
+}
